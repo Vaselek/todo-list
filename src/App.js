9cac850c63454e3e5a8611ca9c6402430ec16c69
Vaselek <burzhuas@gmail.com>
2018-12-20 14:06:26 +0600

    Add state and main logic to app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import AddTaskForm from './components/AddTaskForm/AddTaskForm';
+import Task from './components/Task/Task';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        tasks: [
+            {id: 0, title: 'Clean PC', done: true},
+            {id: 1, title: 'Wash hands', done: false},
+        ],
+        currentTask: 'So what else'
+    };
+
+    onChange = (e) => {
+        this.setState({
+            currentTask: e.target.value
+        })
+    };
+
+    onSubmit = (e) => {
+        const tasks = [...this.state.tasks];
+        tasks.push({id: (this.state.tasks.length + 1), title: this.state.currentTask});
+        this.setState({tasks})
+    };
+
+    removeTask = (index) => {
+        const tasks = [...this.state.tasks];
+        tasks.splice(index, 1);
+        this.setState({tasks});
+    };
+
+    toggleTaskCompleteness = (index) => {
+        const tasks = [...this.state.tasks];
+        const task = tasks[index];
+        task.done = !task.done;
+        debugger;
+        this.setState({tasks});
+    }
+
+    render() {
+        const tasks = this.state.tasks.map((task, index) => {
+            return <Task title={task.title}
+                         done={task.done}
+                         toggleDone={() => this.toggleTaskCompleteness(index)}
+                         remove={() => this.removeTask(index)}/>
+        });
+        return (
+            <div>
+                <AddTaskForm title={this.state.currentTask}
+                             onChange={(e) => this.onChange(e)}
+                             onSubmit={() => this.onSubmit()}/>
+                {tasks}
+            </div>
+        );
+    }
 }
 
 export default App;
